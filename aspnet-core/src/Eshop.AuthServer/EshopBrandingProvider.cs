d762638462cc5bbe539fffd61c99e3657453ebd8
Chien <chien.minh001@gmail.com>
2025-06-24 16:05:51 +0700

    Add project files.

---
+++ aspnet-core/src/Eshop.AuthServer/EshopBrandingProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+using Eshop.Localization;
+using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace Eshop;
+
+[Dependency(ReplaceServices = true)]
+public class EshopBrandingProvider : DefaultBrandingProvider
+{
+    private IStringLocalizer<EshopResource> _localizer;
+
+    public EshopBrandingProvider(IStringLocalizer<EshopResource> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public override string AppName => _localizer["AppName"];
+}
---
+++ aspnet-core/src/Eshop.DbMigrator/EshopDbMigratorModule.cs
@@ -0,0 +1,21 @@
+﻿using Eshop.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.Modularity;
+
+namespace Eshop.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(EshopEntityFrameworkCoreModule),
+    typeof(EshopApplicationContractsModule)
+    )]
+public class EshopDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "Eshop:"; });
+    }
+}
